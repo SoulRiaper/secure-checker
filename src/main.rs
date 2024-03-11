9ec85fdb320a8bf0bf986d57259d1298df5320c0
SoulRiaper <soulriaper@gmail.com>
2024-03-11 16:29:18 +0300

    fix: remove console if running windows

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+#![windows_subsystem = "windows"]
+
 use web_view::*;
+use std::env;
 
 fn main() {
+    let username = match env::var("USERNAME").or_else(|_| env::var("USER")) {
+        Ok(res) => res,
+        Err(_) => "undefined".to_string()
+    };
     web_view::builder()
-        .title("Привет, мир!")
-        .content(Content::Html("<h1>Hello, World!</h1>"))
+        .title("Привет!")
+        .content(Content::Html(format!("<h1>Привет, {}</h1>", username)))
         .size(320, 480)
         .resizable(false)
         .debug(true)
