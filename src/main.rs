fa41a3fc3d739eb97521d463d048258110f7fe59
SoulRiaper <soulriaper@gmail.com>
2024-03-13 22:30:06 +0300

    add logic to handle js invokes + update deps

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 #![windows_subsystem = "windows"]
 
+mod view;
+
 use web_view::*;
 use std::env;
+use view::view::render_main_view;
 
 fn main() {
     let username = match env::var("USERNAME").or_else(|_| env::var("USER")) {
         Ok(res) => res,
         Err(_) => "undefined".to_string()
     };
+    let formatted_html = render_main_view(username, "No policy today".to_string());
+    println!("{}",formatted_html);
+
     web_view::builder()
         .title("Привет!")
-        .content(Content::Html(format!("<h1>Привет, {}</h1>", username)))
-        .size(320, 480)
+        .content(Content::Html(formatted_html))
+        .size(800, 600)
         .resizable(false)
-        .debug(true)
         .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
+        .invoke_handler(|_webview, _arg| {
+            println!("Arg: {}", _arg);
+            match _arg {
+                "user_accept_policy" => {
+                    println!("User accept policy");
+                }
+                "user_reject_policy" => {
+                    println!("User reject policy"); 
+                    _webview.exit();
+                }
+                _ => {
+                    println!("Bad input");
+                }
+            }
+            Ok(())
+        })
+        .build()
+        .unwrap()
         .run()
         .unwrap();
+
+        
 }
