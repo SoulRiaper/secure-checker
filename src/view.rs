02619cb2d593488c764ec7770dfe67e8d60ec683
SoulRiaper <soulriaper@gmail.com>
2024-03-13 22:29:26 +0300

    add view module and main template

---
+++ src/view.rs
@@ -0,0 +1,19 @@
+pub mod view {
+
+    use askama::Template;
+
+    #[derive(Template)]
+    #[template(path = "index.html")]
+    struct MainTemplate {
+        user: String,
+        policy_text: String,
+    }
+
+    pub fn render_main_view(user: String, policy_text: String) -> String {
+        let template = MainTemplate {
+            user,
+            policy_text
+        };
+        return template.render().ok().unwrap_or_default();
+    }
+}
