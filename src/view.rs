059aacefe5319f57141d0ca193efe59b7a0401d2
SoulRiaper <soulriaper@gmail.com>
2024-04-01 12:06:01 +0300

    add: css file reading

--- src/view.rs
+++ src/view.rs
@@ -1,11 +1,13 @@
 pub mod main_view {
 
     use askama::Template;
+    use std::fs;
 
     #[cfg(target_os = "windows")]
     #[derive(Template)]
     #[template(path = "win.index.html")]
     struct MainTemplate {
+        styles: String,
         user: String,
         policy_text: String,
     }
@@ -14,12 +16,15 @@ pub mod main_view {
     #[derive(Template)]
     #[template(path = "index.html")]
     struct MainTemplate {
+        styles: String,
         user: String,
         policy_text: String,
     }
 
     pub fn render_main_view(user: String, policy_text: String) -> String {
+        let styles = fs::read_to_string("styles.css").unwrap_or("".to_string());
         let template = MainTemplate {
+            styles,
             user,
             policy_text
         };
